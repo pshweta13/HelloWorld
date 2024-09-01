d0ab6085dce9d434fd602298c6a07d5222764383
pshweta13 <shweta0098@gmail.com>
2024-09-01 19:05:46 +0400

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Shweta Patel on 01/09/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
